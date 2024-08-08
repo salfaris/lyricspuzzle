721774570daaa454562ce35082d456f30fff6fe9
Salman <salmanfaris98@gmail.com>
2024-08-08 21:10:49 +0100

    make: lyrics better

--- lyrics.py
+++ lyrics.py
@@ -5,8 +5,15 @@ import shutil
 
 import lyricsgenius
 from lyricsgenius import Genius
+from pydantic import BaseModel
 
-LYRICS_LIB = Path(__file__) / "lyrics"
+
+class Song(BaseModel):
+    artist: str
+    title: str
+
+
+LYRICS_LIB = Path(__file__).parents[0] / "lyrics"
 LYRICS_LIB.mkdir(exist_ok=True, parents=True)
 
 try:
@@ -18,19 +25,22 @@ except KeyError:
 GENIUS = Genius(access_token=access_token)
 
 
-def _lyrics_fname_fmt(s):
-    return "_".join(s.lower().split(" "))
+def _to_camel_case(text):
+    words = text.split()
+    camel_case_words = [words[0].lower()] + [word.capitalize() for word in words[1:]]
+    camel_case_text = "".join(camel_case_words)
+    return camel_case_text
 
 
-def find_song(title: str, artist_name: str):
-    song = GENIUS.search_song(title, artist_name)
+def find_song(song: Song) -> lyricsgenius.types.Song:
+    song = GENIUS.search_song(song.title, song.artist)
     return song
 
 
 def save_song(song: lyricsgenius.types.Song):
     fname = "{}_{}".format(
-        _lyrics_fname_fmt(song.artist),
-        _lyrics_fname_fmt(song.title),
+        _to_camel_case(song.artist),
+        _to_camel_case(song.title),
     )
     ext = "txt"
     song.save_lyrics(filename=fname, extension=ext)
